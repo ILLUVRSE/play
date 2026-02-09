b821d6f91b7d75ab05aa158f76205c84f38ed641
Ryan Lueckenotte <ryan.lueckenotte@gmail.com>
2026-02-09 11:28:40 -0600

    chore: remove extra prisma migration

--- prisma/migrations/20260209170116_voice_playlist/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropForeignKey
-ALTER TABLE "PlaylistItem" DROP CONSTRAINT "PlaylistItem_partyId_fkey";
-
--- DropIndex
-DROP INDEX "PlaylistItem_partyId_orderIndex_idx";
-
--- AddForeignKey
-ALTER TABLE "PlaylistItem" ADD CONSTRAINT "PlaylistItem_partyId_fkey" FOREIGN KEY ("partyId") REFERENCES "Party"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
